fa98595de881d0926b83f5ad93df7ad4ccf71e16
hiyokomannjuu <hiyokomanju256kojino@gmail.com>
2026-08-31 01:11:00 +0900

    Make server listen on configurable port
    Updated server to listen on a configurable port and added a log message for the port number.

--- server.js
+++ server.js
@@ -63,7 +63,8 @@ wss.on("connection", (ws) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("ゲームサーバー起動！");
-    console.log("http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`ゲームサーバー起動！ PORT: ${PORT}`);
 });
